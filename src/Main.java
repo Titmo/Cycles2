d6191e21d985640bd875000a6e0f5dd4391c82b4
Danila <03denchik03@gmail.com>
2023-03-22 15:11:00 +0300

    Задания с 4 по 8

--- src/Main.java
+++ src/Main.java
@@ -52,27 +52,69 @@ public class Main {
     }
     public static void task4() {
         System.out.println("Задача 4");
-
+        int contribution = 15_000;
+        int total = 0;
+        int month = 1;
+        while (total <= 12_000_000) {
+            total= (int) (total+contribution+(total*0.07));
+            System.out.println("Месяц " + month + " сумма накоплений равна " + total + " рублей");
+            month++;
+        }
     }
 
     public static void task5() {
         System.out.println("Задача 5");
-
+        int contribution = 15_000;
+        int total = 0;
+        int month = 1;
+        while (total <= 12_000_000) {
+            total= (int) (total+contribution+(total*0.07));
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + " сумма накоплений равна " + total + " рублей");
+            }
+            month++;
+        }
     }
 
     public static void task6() {
         System.out.println("Задача 6");
-
+        int month = 9 * 12;
+        int contribution = 15_000;
+        int total = 0;
+        for (int i = 0; i <= month; i++){
+            total= (int) (total+contribution+(total*0.07));
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+            }
+        }
     }
 
     public static void task7() {
         System.out.println("Задача 7");
+        int dateOfTheMonth = 1;
+        int weekday = 1;
+        while (dateOfTheMonth <= 31) {
+            if (weekday == 5) {
+                System.out.println("Сегодня пятница, " + dateOfTheMonth + "-е число. Необходимо подготовить отчет");
 
+            }
+            dateOfTheMonth++;
+            weekday++;
+            if (weekday == 8) {
+                weekday=1;
+            }
+        }
     }
 
     public static void task8() {
         System.out.println("Задача 8");
-
-
+        int year = 2023;
+        int lastTwoHundredYears = year-200;
+        int futureHundredYears = year+100;
+        for (int i = lastTwoHundredYears; i <= futureHundredYears; i++) {
+            if (i % 79 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
